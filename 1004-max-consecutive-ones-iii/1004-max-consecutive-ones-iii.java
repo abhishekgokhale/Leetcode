75ab310db1b4ee38ec07187c628be52ed228a81d
Abhishek Gokhale <41177970+abhishekgokhale@users.noreply.github.com>
2023-08-24 13:17:53 -0700

    Time: 4 ms (21.54%), Space: 46.3 MB (63.89%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -4,22 +4,11 @@ class Solution {
         int left = 0, right = 0;
         
         while(right < nums.length){
-            // while(k > 0){
-            //     if(right < nums.length){
-            //         if(nums[right] == 1)
-            //             right++;
-            //         else{
-            //             k -= 1
-            //         }
-            //     }
-            // }
-            
             while(right < nums.length && (nums[right] == 1 || k > 0)){
                 if(nums[right] == 0)
                     k--;
                 right++;
                 ans = Math.max(ans, right - left);
-                // System.out.println(right + ", " + left);
             }
             while(right < nums.length && left <= right && nums[left] != 0)
                 left += 1;
