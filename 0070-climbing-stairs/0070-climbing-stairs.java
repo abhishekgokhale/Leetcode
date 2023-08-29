dfe819e86b21d0bb1fc891c64260df0529dd5c01
Abhishek Gokhale <41177970+abhishekgokhale@users.noreply.github.com>
2023-08-29 00:22:16 -0700

    Time: 0 ms (100.00%), Space: 39.6 MB (13.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,22 +1,14 @@
 class Solution {
-    int[] memo;
-    int n;
-    
     public int climbStairs(int n) {
-        this.n = n;
-        this.memo = new int[n+1];
-        return recurse(0);
-    }
-    
-    private int recurse(int i){
-        if(i > n)
-            return 0;
-        if(i == n)
+        if(n == 1)
             return 1;
-        if(memo[i] != 0)
-            return memo[i];
         
-        memo[i] = recurse(i+1) + recurse(i+2);
-        return memo[i];
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        
+        return dp[n];
     }
 }
