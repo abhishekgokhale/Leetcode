74d2df390754795d659db9ed242d7464665e7703
Abhishek Gokhale <41177970+abhishekgokhale@users.noreply.github.com>
2023-08-31 19:09:08 -0700

    Time: 2 ms (65.73%), Space: 44.6 MB (15.14%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,63 +1,54 @@
-class State{
-    int i;
-    int j;
-    State(int i, int j){
-        this.i = i;
-        this.j = j;
-    }
-}
-
 class Solution {
-    char[][] board;
     boolean[][] seen;
+    char[][] board;
+    
     public void solve(char[][] board) {
         this.board = board;
         this.seen = new boolean[board.length][board[0].length];
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(!seen[i][j] && board[i][j] == 'O'){
-                    seen[i][j] = true;
-                    ArrayList<State> capture = getCapturedPlaces(board, i, j);
-                    // for(State s: capture){
-                    //     System.out.println(s.i + ", " + s.j);
-                    // }
-                    for(State s: capture){
-                        board[s.i][s.j] = 'X';
-                    }
-                }
+        
+        for(int i = 0; i < board[0].length; i++)
+            if(board[0][i] == 'O')
+                dfs(0, i, board);
+        // printSeen();
+        for(int i = 0; i < board[0].length; i++)
+            if(board[board.length-1][i] == 'O')
+                dfs(board.length-1, i, board);
+        // printSeen();
+        for(int i = 0; i < board.length; i++)
+            if(board[i][0] == 'O')
+                dfs(i, 0, board);
+        // printSeen();
+        for(int i = 0; i < board.length; i++)
+            if(board[i][board[0].length-1] == 'O')
+                dfs(i, board[0].length-1, board);
+        // printSeen();
+        
+        for(int i = 0; i < seen.length; i++){
+            for(int j = 0; j < seen[0].length; j++){
+                if(board[i][j] == 'O' && !seen[i][j])
+                    board[i][j] = 'X';
             }
         }
     }
     
-    private ArrayList<State> getCapturedPlaces(char[][] board, int i, int j) {
-        ArrayList<State> capture = new ArrayList<>();
-        boolean isCaptured = true;
-        if(i == 0 || i == board.length - 1 || j == 0 || j == board[0].length - 1)
-            isCaptured = false;
-        
-        capture.add(new State(i, j));
-        int[][] directions = {{0,1}, {1,0}, {-1,0}, {0,-1}};
-        
-        Queue<State> queue = new LinkedList<>();
-        queue.add(new State(i, j));
+    // public void printSeen(){
+    //     for(int i = 0; i < seen.length; i++){
+    //         for(int j = 0; j < seen[0].length; j++){
+    //             System.out.print(seen[i][j] + " ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+    
+    public void dfs(int i, int j, char[][] board){
         seen[i][j] = true;
-        while(!queue.isEmpty()){
-            State state = queue.remove();
-            for(int[] dir: directions){
-                int newI = state.i + dir[0], newJ = state.j + dir[1];
-                if(isValid(newI, newJ, board) && !seen[newI][newJ]){
-                    seen[newI][newJ] = true;
-                    if(newI == 0 || newI == board.length - 1 || newJ == 0 || newJ == board[0].length - 1)
-                        isCaptured = false;
-                    capture.add(new State(newI, newJ));
-                    queue.add(new State(newI, newJ));
-                }
+        for(int[] dir: new int[][]{{0,1}, {1,0}, {-1,0}, {0,-1}}){
+            int newI = i + dir[0], newJ = j + dir[1];
+            if(isValid(newI, newJ, board) && !seen[newI][newJ]){
+                dfs(newI, newJ, board);
             }
         }
-        
-        return isCaptured ? capture : new ArrayList<>();
     }
-    
     private boolean isValid(int i, int j, char[][] board){
         return 0 <= i && i < board.length && 0 <= j && j < board[0].length && board[i][j] == 'O';
     }
