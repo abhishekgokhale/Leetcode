2f494dd4386338b9577db5d628d8c0eb17beffa2
Abhishek Gokhale <41177970+abhishekgokhale@users.noreply.github.com>
2023-08-31 23:16:42 -0700

    Time: 0 ms (100.00%), Space: 39.8 MB (57.80%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,22 +1,16 @@
 class Solution {
-    int[] memo;
-    int[] nums;
-    
     public int rob(int[] nums) {
-        this.nums = nums;
-        this.memo = new int[nums.length];
-        for(int i = 0; i < nums.length; i++)
-            memo[i] = -1;
-        return recurse(0);
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        return recurse(0, nums, memo);
     }
     
-    private int recurse(int i){
-        if(i >= nums.length)
-            return 0;
-        if(memo[i] != -1)
-            return memo[i];
+    private int recurse(int i, int[] nums, HashMap<Integer, Integer> memo){
+        if(i >= nums.length) return 0;
         
-        memo[i] = Math.max(nums[i] + recurse(i+2), recurse(i+1));
-        return memo[i];
+        if(memo.containsKey(i)) return memo.get(i);
+        
+        int ans = Math.max(nums[i] + recurse(i+2, nums, memo), recurse(i+1, nums, memo));
+        memo.put(i, ans);
+        return ans;
     }
 }
